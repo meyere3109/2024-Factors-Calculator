bdc7ba216504f0949a913bac19963278fe32a183
meyere3109 <meyere3109@masseyhigh.school.nz>
2024-07-05 14:41:15 +1200

    Working Instructions

---
+++ C_01_Instructions.py
@@ -0,0 +1,27 @@
+# Generates headings
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# Displays instructions
+def instructions(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+    print('''
+Instructions go here.
+- instruction 1
+- instruction 2
+- etc
+    ''')
+
+
+# Main routine goes here
+
+# Display instructions if requested
+want_instructions = input("Press <enter> to read the instructions "
+                          "or any key to continue. ")
+
+if want_instructions == "":
+    instructions("Instructions", "*")
+
+print("program continues")
