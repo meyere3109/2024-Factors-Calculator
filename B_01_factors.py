e6d9a4169299db2bb0dea6dbc1a00234b5b9e96b
meyere3109 <meyere3109@masseyhigh.school.nz>
2024-08-05 14:12:13 +1200

    Working factors calculator

--- B_01_factors.py
+++ B_01_factors.py
@@ -8,9 +8,8 @@ def instructions(statement, decoration):
     print(f"\n{decoration * 5} {statement} {decoration * 5}")
 
     print('''
-Enter a valid number to factor:
-To exit factor calculator enter:
-    - xxx
+    - Enter an integer between 1 and 200 inclusive.
+    - Enter "xxx" to exit.
     ''')
 
 
@@ -18,7 +17,7 @@ To exit factor calculator enter:
 def num_check(question):
     error = "Please enter a whole number that is between 1 and 200 inclusive\n"
     while True:
-
+        # ask user the question, return exit code if it's been entered
         response = input(question).lower()
         if response == "xxx":
             return response
@@ -33,24 +32,75 @@ def num_check(question):
             else:
                 print(error)
 
+        # if users don't enter a number, output the error message
         except ValueError:
             print(error)
 
 
-# Main routine goes here
+def factor_finder(needs_factoring):
+    all_factors = []
 
+    # find square root of number to be factored so that our loop is efficient.
+    stop_raw = needs_factoring ** 0.5
+    stop = int(stop_raw + 1)
+
+    # loop and check each number from one upwards to check if it's a
+    # factor.  Stop when we get to the square root of the number (ie: 'half way')
+    for item in range(1, stop):
+        num_left = needs_factoring % item
+        # If number fits evenly it will add it to the list
+        if num_left == 0:
+            all_factors.append(item)
+
+            # retrieve the second 'partner' factor as an integer
+            partner = to_factor // item
+
+            # only add factor to list if it is not already in the list
+            if partner not in all_factors:
+                all_factors.append(partner)
+
+    # finds out if the number is unity
+    if len(all_factors) == 1:
+        comment = "\n1 is UNITY (has only one factor, itself)."
+
+    # Finds out if the number is a prime number.
+    elif len(all_factors) == 2:
+        comment = f"\n{to_factor} is a prime number."
+
+    # Finds out if the number is a perfect square and outputs it.
+    elif len(all_factors) % 2 == 1:
+        comment = f"\n{to_factor} is a perfect square."
+
+    # If the number is nothing special this outputs it.
+    else:
+        comment = ""
+
+    print(f"{comment}")
+
+    all_factors.sort()
+
+    if len(all_factors) > 1:
+        print(f"The factors of {to_factor} are: {all_factors}\n")
+
+
+# Main routine goes here
 statement_generator("The Ultimate Factor Finder", "-")
 
 # Displays instructions if requested
-want_instructions = input("\n Press <enter> to read the instructions")
-
+want_instructions = input("\nPress <enter> to read the instructions: ")
 
 if want_instructions == "":
     instructions("Instructions", "-")
 
 while True:
-    to_factor = num_check("To factor: ")
-    print("You chose to factor", to_factor)
+
+    # ask the user for the number to be factorised
+    to_factor = num_check("Enter a number to factor: ")
 
     if to_factor == "xxx":
         break
+
+    else:
+        factor_finder(to_factor)
+
+print("Thank you for using the factors calculator")
